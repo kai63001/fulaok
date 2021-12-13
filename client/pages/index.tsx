ce39a0710f0d1ddfa89896796896081b6f92f27b
kai63001 <animehq5@gmail.com>
2021-12-13 20:30:27 +0700

    update i18n change lang

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,19 @@
-import type { NextPage } from 'next'
-import Layout from "@/components/Layout"
+import type { NextPage } from "next";
+import Layout from "@/components/Layout";
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div className="bg-red-600">
-        asdas
-      </div>
+      <div className="bg-red-600">asdas</div>
     </Layout>
-  )
+  );
+};
+
+export async function getServerSideProps(context: any) {
+  // console.log(context.locale);
+  return {
+    props: {},
+  };
 }
 
-export default Home
+export default Home;
