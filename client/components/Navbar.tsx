f038d9b47dc0e8f3b39c0ac7d2477fc19778605b
kai63001 <animehq5@gmail.com>
2021-12-13 21:13:37 +0700

    udpate

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,5 +1,13 @@
-const Navbar = () => {
-  return <nav>test</nav>;
+import { useRouter } from "next/router";
+
+const Navbar = (props: any) => {
+  const { locale = "en" } = useRouter();
+  const data: any = {
+    test: { en: "test", th: "ทดสอบ" },
+  };
+
+  console.log(locale);
+  return <nav>{data["test"][locale.toString()]}</nav>;
 };
 
 export default Navbar;
