642ed1307604cfecc1c3e4c8a5fdf1a71d219352
kai63001 <animehq5@gmail.com>
2021-12-31 18:09:48 +0700

    add key index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,23 +10,11 @@ const Home: NextPage = (props: any) => {
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-5">
         {props.data.map((data: any, i: any) => {
           return (
-            <Card id={data.id} image={data.image}>
+            <Card key={i} id={data.id} image={data.image}>
               {data.title}
             </Card>
           );
         })}
-        {/* <Card image="https://petmaya.com/wp-content/uploads/2021/12/architecture.jpg">
-          20 สถาปัตยกรรมจากประเทศต่าง ๆ ที่ผู้คนต่างปรบมือให้ในความสุดยอด
-        </Card>
-        <Card image="https://petmaya.com/wp-content/uploads/2021/12/ridiculous-tourists.jpg">
-          15 ความเฟลของนักท่องเที่ยวตามสถานที่ท่องเที่ยวต่าง ๆ ทั่วโลก
-        </Card>
-        <Card image="https://petmaya.com/wp-content/uploads/2021/12/architecture.jpg">
-          123123
-        </Card>
-        <Card image="https://petmaya.com/wp-content/uploads/2021/12/architecture.jpg">
-          12312
-        </Card> */}
       </div>
     </Layout>
   );
